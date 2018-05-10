72fe3a9af0deae654a359b6a5f165d07a34b9ced
DianaKiss <dana.butcher@gmail.com>
2018-05-10 12:19:42 +0200

    more two-numbers stuff

--- week-01/day-04/two-numbers.ts
+++ week-01/day-04/two-numbers.ts
@@ -5,7 +5,25 @@
 let o: number = 22;
 let p: number = 13;
 
+// first having some fun
 console.log(o + p);
 console.log(o % p);
 console.log(o ** p);
 console.log(o - p / p + p);
+
+
+
+// Print the result of 13 added to 22
+console.log(p + o);
+
+// Print the result of 13 substracted from 22
+console.log(o - p);
+
+// Print the result of 22 multiplied by 13
+console.log(o * p);
+
+// Print the result of 22 divided by 13 (as a decimal fraction)
+console.log(o / p);
+
+// Print the reminder of 22 divided by 13
+console.log(o % p);
