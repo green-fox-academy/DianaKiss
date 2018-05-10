bd5a0decca150da4ded3db9bdf075777a55ffbb9
DianaKiss <dana.butcher@gmail.com>
2018-05-10 12:08:58 +0200

    adding introduction and 2 numbers

---
+++ week-01/day-04/two-numbers.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+
+let o: number = 22;
+let p: number = 13;
+
+console.log(o + p);
+console.log(o % p);
+console.log(o ** p);
+console.log(o - p / p + p);
