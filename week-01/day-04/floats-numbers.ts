f5d712fcc7ad3aa5958bb2d24961cee91e51e14d
DianaKiss <dana.butcher@gmail.com>
2018-05-10 11:54:24 +0200

    adding numbers

---
+++ week-01/day-04/floats-numbers.ts
@@ -0,0 +1,14 @@
+console.log(3.1415);
+console.log(-1.5);
+console.log(0.0);
+console.log(.5);
+console.log(4.);
+
+// number operators
+
+console.log(1 + 1);
+console.log(3 - 1);
+console.log(3 * 4);
+console.log(12 / 10);
+console.log(5 % 2);
+
