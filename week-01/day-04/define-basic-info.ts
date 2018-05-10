500d9417f77898a5ae9f7146929191ce19256b3b
DianaKiss <dana.butcher@gmail.com>
2018-05-10 14:18:33 +0200

    define basic info excercise

---
+++ week-01/day-04/define-basic-info.ts
@@ -0,0 +1,18 @@
+'use strict';
+export {};
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+var name: string = 'DianaKiss';
+var age: number = 36;
+var height: number = 1.71;
+var ifMarried: boolean = true;
+
+console.log(name);
+console.log(age);
+console.log(height);
+console.log(ifMarried);
