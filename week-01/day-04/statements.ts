a9fe8fa5314af43bce71c8ad95bd78c0417a8e3f
DianaKiss <dana.butcher@gmail.com>
2018-05-10 11:08:48 +0200

    more stuff in TS notes

--- week-01/day-04/statements.ts
+++ week-01/day-04/statements.ts
@@ -1,7 +1,66 @@
-let iLikeCats: boolean = false;
+let iLikeCats: boolean = true;
 
 if (iLikeCats) {
     console.log('I like cats!');
 } else {
     console.log('I dont like cats!');
 }
+
+// here comes for loops :)
+
+/*for (statement1; statement2; statement3) {
+        statement1 is a variable
+        if statement2 is true the loop will start over again
+        statement 3 tells the loop what to do? 
+}
+
+
+let newNumber: number = 0;
+
+for (let i: number = 0; i < 10; i++) {
+    console.log(`${i}`)
+    console.log(`${newNumber += 2}`)
+}
+*/
+
+
+/* we dont really like while but it looks like this
+
+
+let k: number = 0;
+while (k < 10){
+    console.log(`${k}`);
+    k++;
+}
+
+
+do while
+
+
+let m: number = 0;
+do {
+     console.log(`${m}`);
+     m++;
+} while (m < 10)
+
+
+
+switch case
+*/
+
+let sgNew: number = 3;
+switch(sgNew) {
+    case 1:
+        console.log('Hello');
+        break;
+    case 2:
+        console.log('Hy');
+        break;
+    case 3:
+        console.log('Noo');
+        break;
+    default:
+        console.log('this is really false');
+}
+
+
