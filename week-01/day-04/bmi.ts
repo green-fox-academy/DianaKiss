7e28f0d3df4b3ce8796e192e880280b07a7790cc
DianaKiss <dana.butcher@gmail.com>
2018-05-10 14:11:45 +0200

    solving bmi excercise

---
+++ week-01/day-04/bmi.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+// Print the Body mass index (BMI) based on these values
+
+// [weight (kg) / height (m) / height (m)]
+
+
+
+console.log(`BMI = ${massInKg / heightInM / heightInM}`)
+
+console.log(`BMI = ${massInKg / heightInM ** 2}`)
+
+// whoa, it's the same either way
