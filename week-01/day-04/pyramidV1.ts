2a262555cf791413079d8facb010a6bde292174c
DianaKiss <dana.butcher@gmail.com>
2018-05-11 12:10:23 +0200

    pyramid lineCount issue solved

--- week-01/day-04/pyramidV1.ts
+++ week-01/day-04/pyramidV1.ts
@@ -13,14 +13,20 @@ export {};
 //
 // The pyramid should have as many lines as lineCount is
 
-let lineCount: number = 8;
+let lineCount: number = 4;
 
 // code works fine, just needs to expand nr of spaces in variant: pyramid - not so elegant...
-let pyramid: string = '        *';
+let pyramid: string = '*';
 let pyramidVar: string = '**';
+let space: string = ' '
+
+// let's try to set up pyramid space-length
+for (let j: number = 0; j < lineCount; j++) {
+    pyramid = space + pyramid
+}
+
 
 for (let i: number = 0; i < lineCount; i ++) {
-
     console.log(pyramid)
     pyramid = pyramid.slice( 1 ) + pyramidVar;
 }
