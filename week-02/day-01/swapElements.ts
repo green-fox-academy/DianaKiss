662444614d6be37a44e0c2dcd3297d95102714f0
DianaKiss <dana.butcher@gmail.com>
2018-05-14 16:42:59 +0200

    pushing swap elements

---
+++ week-02/day-01/swapElements.ts
@@ -0,0 +1,15 @@
+'use strict'
+export {};
+
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+
+let abc: string [] = ["Arthur", "Boe", "Chloe"];
+
+console.log(abc);
+
+abc = ["Chloe", "Boe", "Arthur"];
+
+console.log(abc);
+
+//this seems too easy to be a solution
