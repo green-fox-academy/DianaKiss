e93652e215d369533ab57f2ce0e26a51d2265c44
DianaKiss <dana.butcher@gmail.com>
2018-05-15 10:57:53 +0200

    changeElements colser to finishing. not finished yet

---
+++ week-02/day-01/changeElements.ts
@@ -0,0 +1,22 @@
+'use strict'
+export {};
+
+
+// -  Create an array named `numList` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 with the `.map` method 
+// -  Print the fourth element as a test
+
+let numList: number [] = [1, 2, 3, 8, 5, 6];
+
+console.log(numList);
+
+let updateNumList = numList.map((num: number, index: number) =>  {
+  if (num !== index + 1) { 
+    num = num / 2;
+  }
+
+  return num;
+});
+
+console.log(updateNumList);
+
