748df1033b94111dc183f01171614e98848c4627
DianaKiss <dana.butcher@gmail.com>
2018-05-17 18:25:27 +0200

    chess pattern, I like it multicolored so I will leave it

---
+++ week-02/day-04/13-chess/checkerboard-ts.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Fill the canvas with a checkerboard pattern.
+
+function drawSquare(startX: number, startY: number, size: number, ) {
+  ctx.fillRect(startX, startY, size, size);
+}
+
+for (let i: number = 0; i < 9; i ++) {
+  for (let j: number = 0; j < 9; j ++) {
+    ctx.fillStyle = 'khaki';
+    drawSquare(i * 100, j  * 100, 50);
+    ctx.fillStyle = 'green';
+    drawSquare(i * 100 + 50, j  * 100 + 50, 50);
+  }
+}
