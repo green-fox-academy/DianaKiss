d192cf18c6a10dc651768bad8e4d4907dae8515a
DianaKiss <dana.butcher@gmail.com>
2018-05-29 14:26:17 +0200

    GF inheritance excercise

---
+++ week-04/day-02/GF-inheritace/person.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+class Person {
+  protected name: string;
+  protected age: number;
+  protected gender: string;
+
+  public constructor(name: string = 'Jane Doe', age: number = 30, gender: string = 'female') {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  public introduce(): void {
+    console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender}.`);
+  }
+
+  public getGoal(): void {
+    console.log('My goal is: Live for the moment!');
+  }
+}
+
+export {Person};
