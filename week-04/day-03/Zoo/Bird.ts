36c82aaef687310ff5ba27056c481a791fc636bd
DianaKiss <dana.butcher@gmail.com>
2018-05-31 23:15:54 +0200

    still implementing Flyable on Bird

--- week-04/day-03/Zoo/Bird.ts
+++ week-04/day-03/Zoo/Bird.ts
@@ -3,7 +3,7 @@
 import { Animal } from './Animal';
 import { Flyable } from '../Flyable/Flyable';
 
-class Bird extends Animal {
+class Bird extends Animal implements Flyable{
   constructor(name: string, age: number = 0, gender?: string, skinType: string = 'Feathers', sound: string = 'Twerp, Twerp!') {
     super(name, age, gender, skinType, sound);
     
