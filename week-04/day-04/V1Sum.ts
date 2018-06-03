a59cefffc4e2587dbb7106ed9e5356b061ecdadc
DianaKiss <dana.butcher@gmail.com>
2018-06-03 19:29:23 +0200

    deleting commented sections.

--- week-04/day-04/V1Sum.ts
+++ week-04/day-04/V1Sum.ts
@@ -2,21 +2,6 @@
 
 let probeSum: number[] = [1, 2, 3, 4];
 
-/*
-class Summarizer {
-
-
-  public summethod() {
-    for (let i: number = 0; i < probeSum.length; i++) {
-      valueSum = valueSum + probeSum[i];
-
-    }
-    return valueSum;
-  }
-}
-*/
-
-
 function summethod(anyArray) {
   let valueSum: number = 0;
   for (let i: number = 0; i < anyArray.length; i++) {
@@ -28,5 +13,3 @@ function summethod(anyArray) {
 console.log(summethod(probeSum));
 
 export { summethod };
-
-//export { Summarizer };
