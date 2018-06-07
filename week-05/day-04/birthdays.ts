8a4cfab8a61b644dbe3cbe8a069343f95d1c516c
DianaKiss <dana.butcher@gmail.com>
2018-06-07 13:53:04 +0200

    birthyear counter working (note 2016 and 2006 has the same counts)

--- week-05/day-04/birthdays.ts
+++ week-05/day-04/birthdays.ts
@@ -1,10 +1,9 @@
 'use strict';
-export {};
+export { };
 
 declare function require(path: string): any;
 let fs = require('fs');
 let charEncoding = 'utf-8';
-
 
 function readFile(filename): string {
   try {
@@ -15,13 +14,34 @@ function readFile(filename): string {
   }
 }
 
-
 function birthdays(filename: string) {
 
   let fileContent: string = readFile(filename);
-  let peopleArr: string[] = (fileContent.split(/[;\n\r]+/));
+  let peopleArr: any[] = (fileContent.split(/[-;\n\r]+/));
+  let dateArr: number[] = [];
 
-  return peopleArr;
+  for (let i: number = 0; i < peopleArr.length; i++) {
+    if (i % 5 === 1) {
+      dateArr.push(peopleArr[i]);
+    }
+  }
+    
+  dateArr = dateArr.sort();
+  let counts: object = {};
+  for (let i = 0; i < dateArr.length; i++) {
+      let date: number = dateArr[i];
+      counts[date] = counts[date] ? counts[date] + 1 : 1;
+  }
+
+  let final: any[][] = []
+  for (let year in counts) {
+    final.push([year, counts[year]]);
+    final.sort(function (a, b) {
+      return a[1] - b[1];
+    });
+  }
+
+ return final[final.length -1][0];
 
 }
 
