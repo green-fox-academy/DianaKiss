9b376c0098d13346a3db7184899effb694f393c9
DianaKiss <dana.butcher@gmail.com>
2018-06-21 13:45:25 +0200

    initial commit after matreview

---
+++ week-06/day-04-templates/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// home page
+app.get('/', (req, res) => {
+  // render `home.ejs`
+  res.render('home', {
+    title: 'Hello Green Fox!', 
+    text: 'this is some text here...',
+    showText: false,
+  });
+});
+
+// start express app on port 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
