94136e813ad87591ad32269fe3ee97bfe0529db4
DianaKiss <dana.butcher@gmail.com>
2018-06-26 19:02:35 +0200

    homework

---
+++ week-07/day-03/matreview/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//app.js
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'user',
+  password: 'password',
+  database: 'database name'
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
