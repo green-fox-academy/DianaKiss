4434a1b943a175e6d4b59e2d5af6992c7d9d6452
DianaKiss <dana.butcher@gmail.com>
2018-06-26 21:19:32 +0200

    committing fruits done

---
+++ week-07/day-02/callback/fruits.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Write a program that prints the following fruits:
+// apple -> immediately
+// pear -> after 1 seconds
+// melon -> after 3 seconds
+// grapes -> after 5 seconds
+
+
+function printFruit(fruit) {
+  console.log(fruit);
+}
+
+printFruit('apple');
+setTimeout(() => printFruit('pear'), 1000);
+setTimeout(() => printFruit('melon'), 3000);
+setTimeout(() => printFruit('grapes'), 5000);
