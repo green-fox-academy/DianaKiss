8dd3842e99f1402cb85ac42d3c9fa22af7a38ffb
DianaKiss <dana.butcher@gmail.com>
2018-06-27 14:05:19 +0200

    bookstore initial commit

---
+++ week-07/day-03/bookstore/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+//require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const PORT = 3000;
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  // password: ,
+  database: 'bookstore',
+});
+
+app.get('/booknames', (req, res) => {
+  let sql = 'SELECT book_name from book_mast;'
+
+  conn.query(sql, (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+      return;
+    }
+
+
+    res.json({
+      book_names: rows,
+    });
+  });
+});
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
