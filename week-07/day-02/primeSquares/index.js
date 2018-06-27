71cc13b594bcc9d3d2cbc2bd1eaf2d32fb63ee8b
DianaKiss <dana.butcher@gmail.com>
2018-06-27 19:33:53 +0200

    prime squares initial commit

--- week-07/day-02/primeSquares/index.js
+++ week-07/day-02/primeSquares/index.js
@@ -7,6 +7,13 @@ const PORT = 3000;
 app.use('/static', express.static('static'));
 app.set('view engine', 'ejs');
 
+function counter() {
+  
+  for(let i =0; i < 100; i++) {
+    return i;
+  }
+}
+
 app.get('/', (req, res) => {
   res.render('index', {
 
