8bba419af5c0311693b1079270b799fe63e15f9b
DianaKiss <dana.butcher@gmail.com>
2018-07-02 13:45:39 +0200

    this task is extra hard for those who has not touched cards for more than 20 years

--- week-08/day-01/Dojo/DiaPoker.js
+++ week-08/day-01/Dojo/DiaPoker.js
@@ -2,12 +2,26 @@
 
 const cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 const colors = ['H', 'S', 'C', 'D'];
+//H heart //S sapde //C club //D diamond
 
 
-module.exports = function whoIsTheWinner() {
+module.exports = function whoIsTheWinner(blackHand, whiteHand) {
   let blackCardIndex = 0;
   let whiteCardIndex = 0;
 
-  return 'White wins';
+  // console.log(blackHand);
+
+  for (let i = 0; i < 5; i++) {
+    if (blackCardIndex < blackHand[i][0]) {
+      blackCardIndex = blackHand[i][0];
+    }
+    if (whiteCardIndex < whiteHand[i][0]) {
+      whiteCardIndex = whiteHand[i][0];
+    }
+    console.log(whiteCardIndex);
+    console.log(blackCardIndex);
+  }
+
+  // return 'White wins';
 
 }
--- week-08/day-01/Dojo/DiaTestPoker.js
+++ week-08/day-01/Dojo/DiaTestPoker.js
@@ -7,7 +7,7 @@ test('first test', (t) => {
 
 const blackHand = [[2, 'H'], [3, 'D'], [5, 'S'], [9, 'C'], ['K', 'D']];
 const whiteHand = [[2, 'C'], [3, 'H'], [4, 'S'], [8, 'C'], ['A', 'H']];
-const actual = whoIsTheWinner();
+const actual = whoIsTheWinner(blackHand, whiteHand);
 const expected = 'White wins';
 
 
