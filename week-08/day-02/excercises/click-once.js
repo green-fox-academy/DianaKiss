0a3aa0b4b253f9e0e88c109b0ac2d367103adc4d
DianaKiss <dana.butcher@gmail.com>
2018-07-03 19:11:38 +0200

    refactoring and correcting click once

--- week-08/day-02/excercises/click-once.js
+++ week-08/day-02/excercises/click-once.js
@@ -6,19 +6,18 @@ function getRandomColorNum() {
 }
 
 // first solution disabling buton on the first click
-// button.addEventListener('click', (event) => {
-//   console.log(event.timeStamp);
-//   document.querySelector('html').style.background = `rgb(0, ${getRandomColorNum()}, ${getRandomColorNum()})`;
-//   document.querySelector('button').setAttribute.disabled = true;
-// });
+button.addEventListener('click', (event) => {
+  console.log(event.timeStamp);
+  document.querySelector('html').style.background = `rgb(0, ${getRandomColorNum()}, ${getRandomColorNum()})`;
+  document.querySelector('button').disabled = true;
+});
 
 // second solution is not disabling the button
-let clickCounter = 0;
-button.addEventListener('click', (event) => {
-  clickCounter = clickCounter + 1;
-  if (clickCounter < 2) {
-    console.log(event.timeStamp);
-    document.querySelector('html').style.background = `rgb(0, ${getRandomColorNum()}, ${getRandomColorNum()})`;
-    document.querySelector('button').setAttribute.disabled = true;
-  }
-});
+// let clickCounter = 0;
+// button.addEventListener('click', (event) => {
+//   clickCounter = clickCounter + 1;
+//   if (clickCounter < 2) {
+//     console.log(event.timeStamp);
+//     document.querySelector('html').style.background = `rgb(0, ${getRandomColorNum()}, ${getRandomColorNum()})`;
+//   }
+// });
