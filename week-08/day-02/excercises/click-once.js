baa96f07c41d2219effbba7781aaeaf2b0324b3e
DianaKiss <dana.butcher@gmail.com>
2018-07-03 16:08:28 +0200

    second solution for click once

--- week-08/day-02/excercises/click-once.js
+++ week-08/day-02/excercises/click-once.js
@@ -5,10 +5,20 @@ function getRandomColorNum() {
   return Math.floor(Math.random() * 255);
 }
 
+// first solution disabling buton on the first click
+// button.addEventListener('click', (event) => {
+//   console.log(event.timeStamp);
+//   document.querySelector('html').style.background = `rgb(0, ${getRandomColorNum()}, ${getRandomColorNum()})`;
+//   document.querySelector('button').setAttribute.disabled = true;
+// });
+
+// second solution is not disabling the button
+let clickCounter = 0;
 button.addEventListener('click', (event) => {
-  console.log(event.timeStamp);
-  document.querySelector('html').style.background = `rgb(0, ${getRandomColorNum()}, ${getRandomColorNum()})`;
-  document.querySelector('button').disabled = true;
+  clickCounter = clickCounter + 1;
+  if (clickCounter < 2) {
+    console.log(event.timeStamp);
+    document.querySelector('html').style.background = `rgb(0, ${getRandomColorNum()}, ${getRandomColorNum()})`;
+    document.querySelector('button').setAttribute.disabled = true;
+  }
 });
-
-
