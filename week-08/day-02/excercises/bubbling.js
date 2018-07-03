e4404b90cd0d6f2a3086a3cb4b52af18c4691893
DianaKiss <dana.butcher@gmail.com>
2018-07-03 19:12:10 +0200

    initial commit of bubbling

---
+++ week-08/day-02/excercises/bubbling.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const move = document.querySelector('.move'); //data-action = move
+const zoom = document.querySelector('.zoom'); //data-action = zoom
+
+move.addEventListener('click', (event) => {
+  console.log(event);
+
+
+});
