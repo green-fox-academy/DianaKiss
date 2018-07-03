00a507ceb02a636c2cd89c3400cbe6001c573c4b
DianaKiss <dana.butcher@gmail.com>
2018-07-03 13:55:31 +0200

    Tatiana and me had some fun with this excercise

--- week-08/day-02/excercises/events-2.js
+++ week-08/day-02/excercises/events-2.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const button = document.querySelector('button');
+const kittycat = document.querySelector('button');
 const liArr = document.querySelectorAll('li');
 
-button.addEventListener('click', (event) => {
-  console.log(liArr.length);
+kittycat.addEventListener('click', () => {
+  // console.log(liArr);
   document.querySelector('.result').innerText = liArr.length;
 });
