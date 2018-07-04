b6d53fde8a51f47ca58903fef6dc60c76332639e
DianaKiss <dana.butcher@gmail.com>
2018-07-04 12:00:36 +0200

    adding some more stuff

--- week-08/day-02/excercises/bubbling.js
+++ week-08/day-02/excercises/bubbling.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const move = document.queryselector('[data-action]'); //data-action = move
+const move = document.querySelector('[data-action]'); //data-action = move
 // const zoom = document.querySelector('.zoom'); //data-action = zoom
 
 console.log(move);
@@ -8,4 +8,11 @@ console.log(move);
 
 move.addEventListener('click', (event) => {
   console.log(event);
+
+  switch (event) {
+    case event:
+
+    break;
+    case event:
+  }
 }); 
