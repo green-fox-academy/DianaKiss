da6110b087d42348b9e0b49825fa518dc00cc111
DianaKiss <dana.butcher@gmail.com>
2018-07-04 22:18:46 +0200

    button enabled on selecting pet / if statement is not working yet

--- week-08/day-03/Forms/pets.js
+++ week-08/day-03/Forms/pets.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// const loveCats = document.querySelector('.loveCats');
+// const signUp = document.querySelector('.signUp');
+
+document.querySelector('.fav-type').addEventListener('click', (e) => {
+
+  document.querySelector('.signUp').disabled = false;
+  // e.preventDefault();
+  console.log(document.querySelectorAll('[value="Dog"]'));
+});
+
+document.querySelector('.signUp').addEventListener('click', (e) => {
+  if (e.target.elements.fav-type.value == "Dog" || e.target.elements.fav-type.value == "Cat") {
+  alert('Thank you, you\'ve successfully signed up for cat facts');
+  // e.preventDefault();
+  } else {
+  alert('Sigh, we still added you to the cat facts list');
+
+  }
+});
