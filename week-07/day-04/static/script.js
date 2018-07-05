2168f95bcd13dd984d3bc2469089e2b2af012889
DianaKiss <dana.butcher@gmail.com>
2018-07-05 16:44:11 +0200

    frontend is slowly building up

--- week-07/day-04/static/script.js
+++ week-07/day-04/static/script.js
@@ -1,2 +1,28 @@
 'use strict';
 
+console.log('kiscica');
+
+//ide kene xml http requesteket kesziteni
+
+// get /api/posts mint pl a giphy
+
+const http = new XMLHttpRequest();
+http.open('GET', 'http://localhost:3000/api/posts/' )
+
+http.onload = () => {
+  let response = JSON.parse(http.responseText);
+  console.log(response);
+
+  response.posts.forEach(element => {
+    console.log(element.title);
+    // const div = document.createElement('div');
+    // const image = document.createElement('img');
+
+    // image.src = element.images.fixed_width_still.url
+
+    // container.appendChild(image);
+  });
+
+} 
+
+http.send();
