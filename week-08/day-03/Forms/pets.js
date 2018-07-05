37d8f12b5ae4329addaf4903d486832d933e4419
DianaKiss <dana.butcher@gmail.com>
2018-07-05 08:33:08 +0200

    Different signup messages are working fine

--- week-08/day-03/Forms/pets.js
+++ week-08/day-03/Forms/pets.js
@@ -7,15 +7,15 @@ document.querySelector('.fav-type').addEventListener('click', (e) => {
 
   document.querySelector('.signUp').disabled = false;
   // e.preventDefault();
-  console.log(document.querySelectorAll('[value="Dog"]'));
+  // console.log(document.querySelectorAll('[value="Dog"]'));
 });
 
 document.querySelector('.signUp').addEventListener('click', (e) => {
-  if (e.target.elements.fav-type.value == "Dog" || e.target.elements.fav-type.value == "Cat") {
+  // console.log(document.querySelector('input[name="pet-list"]:checked').value);
+  if (document.querySelector('input[name="pet-list"]:checked').value == "Dog" || document.querySelector('input[name="pet-list"]:checked').value == "Cat" ) {
   alert('Thank you, you\'ve successfully signed up for cat facts');
   // e.preventDefault();
   } else {
   alert('Sigh, we still added you to the cat facts list');
-
   }
 });
