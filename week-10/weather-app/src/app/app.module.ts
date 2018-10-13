b514b00d60bb1b28112aabf306d3a748f3c57e28
DianaKiss <dana.butcher@gmail.com>
2018-10-13 10:36:55 +0200

    Adding weatherData service

--- week-10/weather-app/src/app/app.module.ts
+++ week-10/weather-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LocationTilesComponent } from './location-tiles/location-tiles.component';
@@ -12,7 +13,8 @@ import { SearchLocationComponent } from './search-location/search-location.compo
     SearchLocationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
